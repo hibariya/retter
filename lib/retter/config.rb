22f8bb4e1b518e0a6254118ed3e37acda6dcb247
hibariya <celluloid.key@gmail.com>
2011-09-09 12:59:03 +0900

    rewritable configuration

--- lib/retter/config.rb
+++ lib/retter/config.rb
@@ -1,7 +1,25 @@
 # coding: utf-8
 
 class Retter::Config
-  [:editor, :retter_home, :title, :description, :url, :author].each do |att|
+  [:editor,
+   :title,
+   :description,
+   :url,
+   :author,
+   :retters_dir,
+   :wip_file,
+   :layouts_dir,
+   :layout_file,
+   :profile_layout_file,
+   :entry_layout_file,
+   :entries_layout_file,
+   :index_layout_file,
+   :entries_dir,
+   :profile_file,
+   :index_file,
+   :entries_file,
+   :feed_file
+  ].each do |att|
     class_eval <<-EOM
       def #{att}(val = nil)
         val ? @options[:#{att}] = val : @options[:#{att}]
@@ -9,81 +27,49 @@ class Retter::Config
     EOM
   end
 
+  attr_reader :retter_home
+
   def initialize(env)
     @env, @options = env, {}
 
     detect_retter_home
     raise Retter::EnvError unless env.values_at('EDITOR', 'RETTER_HOME').all?
 
-    editor env['EDITOR']
-    retter_home Pathname.new(env['RETTER_HOME'])
-    url 'http://example.com'
+    @retter_home = Pathname.new(@env['RETTER_HOME'])
+    load_defaults
 
     retterfile = retter_home.join('Retterfile')
     instance_eval retterfile.read if retterfile.exist?
   end
 
+  def load_defaults
+    editor              @env['EDITOR']
+    url                 'http://example.com'
+    retters_dir         retter_home.join('retters/')
+    wip_file            retters_dir.join('today.md')
+    layouts_dir         retter_home.join('layouts/')
+    layout_file         layouts_dir.join('retter.html.haml')
+    profile_layout_file layouts_dir.join('profile.html.haml')
+    entry_layout_file   layouts_dir.join('entry.html.haml')
+    entries_layout_file layouts_dir.join('entries.html.haml')
+    index_layout_file   layouts_dir.join('index.html.haml')
+    entries_dir         retter_home.join('entries/')
+    profile_file        retter_home.join('profile.html')
+    index_file          retter_home.join('index.html')
+    entries_file        retter_home.join('entries.html')
+    feed_file           retter_home.join('entries.rss')
+  end
+
   def detect_retter_home
+    # TODO こういうの上のディレクトリも見て判断するのを何か参考にして書く
     @env['RETTER_HOME'] = Dir.pwd if File.exist? 'Retterfile'
-  end
-
-  def retters_dir
-    retter_home.join 'retters/'
-  end
-
-  def wip_file
-    retters_dir.join 'today.md'
   end
 
   def retter_file(date)
     retters_dir.join(date ? date.strftime("%Y%m%d.md") : "today.md")
   end
 
-  def layouts_dir
-    retter_home.join 'layouts/'
-  end
-
-  def layout_file
-    layouts_dir.join 'retter.html.haml'
-  end
-
-  def profile_layout_file
-    layouts_dir.join 'profile.html.haml'
-  end
-
-  def entry_layout_file
-    layouts_dir.join 'entry.html.haml'
-  end
-
-  def entries_layout_file
-    layouts_dir.join 'entries.html.haml'
-  end
-
-  def index_layout_file
-    layouts_dir.join 'index.html.haml'
-  end
-
-  def entries_dir
-    retter_home.join 'entries/'
-  end
-
   def entry_file(date)
     entries_dir.join date.strftime('%Y%m%d.html')
-  end
-
-  def profile_file
-    retter_home.join 'profile.html'
-  end
-
-  def index_file
-    retter_home.join 'index.html'
-  end
-
-  def entries_file
-    retter_home.join 'entries.html'
-  end
-
-  def feed_file
-    retter_home.join 'entries.rss'
   end
 end
