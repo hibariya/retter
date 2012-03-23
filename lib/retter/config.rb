83f768fa7d85f4e24c56bdcb209061a7a1429e4e
hibariya <celluloid.key@gmail.com>
2012-03-24 00:21:12 +0900

    refactoring

--- lib/retter/config.rb
+++ lib/retter/config.rb
@@ -13,36 +13,15 @@ module Retter
     def_delegators Entries, :renderer, :retters_dir, :wip_file
     def_delegators Pages,   :layouts_dir, :entries_dir, :allow_binding
 
-    ATTRIBUTES = [
-      :editor,
-      :shell,
-      :cache,
-      :title,
-      :description,
-      :url,
-      :author
-    ] + [ # extras
-      :disqus_shortname
-    ]
-
-    ATTRIBUTES.each do |att|
-      class_eval <<-EOM
-        def #{att}(val = nil)
-          val ? @options[:#{att}] = val : @options[:#{att}]
-        end
-      EOM
-    end
-
     attr_reader :retter_home
 
     def initialize(env)
-      @env, @options = env, {}
+      @env             = env
       @after_callbacks = {}
+      @attributes      = {}
 
       detect_retter_home
-      unless env.values_at('EDITOR', 'RETTER_HOME').all?
-        raise EnvError, 'Set $RETTER_HOME and $EDITOR, first.'
-      end
+      environments_required
 
       @retter_home = Pathname.new(@env['RETTER_HOME'])
       load_defaults
@@ -55,18 +34,45 @@ module Retter
       exit 1
     end
 
+    def after_callback(name, sym = nil, &block)
+      if callback = sym || block
+        @after_callbacks[name] = callback
+      else
+        @after_callbacks[name]
+      end
+    end
+
+    alias_method :after, :after_callback
+
+    [ # base
+      :editor, :shell, :cache, :title, :description, :url, :author,
+      # extra
+      :disqus_shortname
+    ].each do |att|
+      class_eval <<-EOM
+        def #{att}(val = nil)
+          val ? @attributes[:#{att}] = val : @attributes[:#{att}]
+        end
+      EOM
+    end
+
+    private
+
+    def detect_retter_home
+      @env['RETTER_HOME'] = Dir.pwd if File.exist? 'Retterfile'
+    end
+
     def load_defaults
-      editor              @env['EDITOR']
-      shell               @env['SHELL']
-      url                 'http://example.com'
+      editor @env['EDITOR']
+      shell  @env['SHELL']
+      url    'http://example.com'
 
-      renderer            Retter::Renderers::CodeRayRenderer
-      retters_dir         retter_home.join('retters/')
-      wip_file            retters_dir.join('today.md')
+      renderer    Retter::Renderers::CodeRayRenderer
+      retters_dir retter_home.join('retters/')
+      wip_file    retters_dir.join('today.md')
 
-      layouts_dir         retter_home.join('layouts/')
-      entries_dir         retter_home.join('entries/')
-
+      layouts_dir retter_home.join('layouts/')
+      entries_dir retter_home.join('entries/')
 
       cache_dir = retter_home.join('tmp/cache')
       cache ActiveSupport::Cache::FileStore.new(cache_dir.to_path)
@@ -80,18 +86,10 @@ module Retter
       instance_eval retterfile.read, retterfile.to_path if retterfile.exist?
     end
 
-    def detect_retter_home
-      @env['RETTER_HOME'] = Dir.pwd if File.exist? 'Retterfile'
-    end
-
-    def after_callback(name, sym = nil, &block)
-      if callback = sym || block
-        @after_callbacks[name] = callback
-      else
-        @after_callbacks[name]
+    def environments_required
+      unless @env.values_at('EDITOR', 'RETTER_HOME').all?
+        raise EnvError, 'Set $RETTER_HOME and $EDITOR, first.'
       end
     end
-
-    alias_method :after, :after_callback
   end
 end
