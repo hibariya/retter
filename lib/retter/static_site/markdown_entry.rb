ae32c638a8601b4c1041da7f0ff649c6f8b29376
hibariya <celluloid.key@gmail.com>
2014-02-09 16:12:10 +0900

    Use mattr_accessor

--- lib/retter/static_site/markdown_entry.rb
+++ lib/retter/static_site/markdown_entry.rb
@@ -5,24 +5,55 @@ require 'nokogiri'
 module Retter
   module StaticSite
     module MarkdownEntry
-      class << self
-        attr_accessor :source_path, :renderer
-        attr_writer :wip_file
-
-        def wip_file
-          @wip_file || source_path.try(:join, 'today.md')
-        end
-
-        def markdown
-          @markdown ||= Markdown.new(renderer || Markdown::Renderer)
-        end
-      end
+      mattr_accessor :source_path, :wip_file, :markdown
 
       extend ActiveSupport::Concern
 
+      def load(path)
+        @source_path = Pathname(path)
+
+        fname = source_path.basename('.*').to_s
+        @date = Entry::Utils.parse_date(fname)
+
+        load_markdown
+      end
+
+      def commit!(date = Date.today)
+        raise 'Already committed' unless wip?
+
+        new_path = source_path.dirname.join(date.strftime('%Y%m%d.md'))
+        source_path.rename new_path
+        @source_path =     new_path
+      end
+
+      def wip?
+        source_path == MarkdownEntry.wip_file
+      end
+
+      private
+
+      def load_markdown
+        html     = Nokogiri::HTML(MarkdownEntry.markdown.render(source_path.read))
+        elements = html.search('body > *').to_a
+
+        @lede     = ''
+        @articles = elements.each.with_object([]) {|el, articles|
+          if el.name == 'h1'
+            articles << Entry::Article.new(entry: self, id: "a#{articles.size}", title: el.text, body: '')
+          else
+            if articles.empty?
+              @lede << el.to_s
+              next
+            end
+
+            articles.last.body << el.to_s
+          end
+        }
+      end
+
       module ClassMethods
         def load
-          return false unless MarkdownEntry.source_path
+          return unless MarkdownEntry.source_path
 
           load_entries
           load_wip_entry
@@ -31,13 +62,13 @@ module Retter
         end
 
         def generate_entry_path(keyword)
-          if entry = find_by_keyword(keyword)
-            entry.source_path
+          if found_entry = find_by_keyword(keyword)
+            found_entry.source_path
           elsif date = Entry::Utils.parse_date(keyword)
             MarkdownEntry.source_path.join(date.strftime('%Y%m%d.md'))
           else
-            if entry = find(Date.today)
-              entry.source_path
+            if today_entry = find(Date.today)
+              today_entry.source_path
             else
               MarkdownEntry.wip_file
             end
@@ -48,70 +79,30 @@ module Retter
 
         def load_entries
           wip_file_path = MarkdownEntry.wip_file.to_path
+          except_wip    = Dir.glob("#{MarkdownEntry.source_path}/*.md").reject {|path| path == wip_file_path }
 
-          self.all =
-            Dir.glob(MarkdownEntry.source_path.join('*.md')).reject {|path|
-              path == wip_file_path
-            }.map {|path|
-              new.tap {|e| e.load path }
-            }
+          self.all = except_wip.map {|path|
+            new.tap {|e| e.load path }
+          }
         end
 
         def load_wip_entry
-          if MarkdownEntry.wip_file.exist?
-            wip_entry = new.tap {|e| e.load(MarkdownEntry.wip_file) }
+          return unless MarkdownEntry.wip_file.exist?
 
-            all << wip_entry unless find(wip_entry.date)
+          wip_entry = new.tap {|e| e.load(MarkdownEntry.wip_file) }
+
+          if find(wip_entry.date)
+            # this file won't read while find(Date.today)
+          else
+            all << wip_entry
           end
         end
       end
 
-      def load(path)
-        @source_path = Pathname(path)
-
-        fname = source_path.basename('.*').to_s
-        @date = Entry::Utils.parse_date(fname)
-
-        load_content
-      end
-
-      def commit!(date = Date.today)
-        raise 'Already committed' unless wip?
-
-        new_source = source_path.dirname.join(date.strftime('%Y%m%d.md'))
-
-        source_path.rename new_source
-        @source_path = new_source
-      end
-
-      def wip?
-        source_path == MarkdownEntry.wip_file
-      end
-
-      private
-
-      def load_content
-        md   = source_path.read
-        html = Nokogiri::HTML(MarkdownEntry.markdown.render(md))
-
-        @lede        = ''
-        @articles    = html.search('body > *').to_a.each.with_index.with_object([]) {|(el, i), articles|
-          if el.name == 'h1'
-            articles << Entry::Article.new(entry: self, id: "a#{articles.size}", title: el.text, body: '')
-          else
-            if articles.empty?
-              @lede << el.to_s
-            else
-              articles.last.body << el.to_s
-            end
-          end
-        }
-      end
-
       Retter.on_initialize do |config|
-        self.renderer    = config.renderer
-        self.wip_file    = config.wip_file
         self.source_path = config.source_path.try(:join, 'retters')
+        self.wip_file    = source_path.try(:join, 'today.md')
+        self.markdown    = Markdown.new(config.renderer || Markdown::Renderer)
 
         -> { Retter::Entry.load }
       end
